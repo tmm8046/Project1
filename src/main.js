d058c203a0da0adefa011a81545f40699f45459d
Tommy Murray Jr <tmm8046@g.rit.edu>
2022-10-01 09:42:43 -0400

    Added Styles and json

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-let word;
+const row = 5;
+const column = 5;
+let pattern = "1, 2, 4, 8, 16, 32, 64, 128, 256, 512";
 
-const grid = new array(5).fill(new array(5));
+const grid = new array(row).fill(new array(column));
+
+//Resets the pattern daily
+const reset = () => {
+
+}
+
+//Draws the grid
+function drawGrid() {
+    for (let i = 0; i < row; i++){
+        for (let i = 0; i < column; i++){
+            let gridBox = document.createElement("div");
+            gridBox.class = "tile";
+            grid.push(gridBox);
+            document.querySelector("#grid").appendChild(gridBox);
+        }
+    }
+}
+
+const init = () => {
+    drawGrid;
+}
+
+window.onload = init;
