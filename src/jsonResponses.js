52f20d3618ac45e043c5a6d478130346aedb51b7
Tommy Murray Jr <tmm8046@g.rit.edu>
2022-10-06 20:47:46 -0400

    JSON code

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,24 +1,51 @@
-const patterns = require("../json/patterns.json");
+const patterns = require('../json/patterns.json');
 
 const respondJSON = (request, response, status, object) => {
   const headers = {
     'Content-Type': 'application/json',
   };
-  
+
   response.writeHead(status, headers);
   response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
   response.end();
 };
 
 const getPattern = (request, response) => {
   const responseJSON = {
     patterns,
-  }
+  };
 
   return respondJSON(request, response, 200, responseJSON);
-}
+};
+
+const getPatternMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
 
 module.exports = {
   respondJSON,
-  getPattern
+  respondJSONMeta,
+  getPattern,
+  getPatternMeta,
+  notFound,
+  notFoundMeta,
 };
--- src/server.js
+++ src/server.js
@@ -6,17 +6,41 @@ const jsonHandler = require('./jsonResponses');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const handleGet = (request, response, parsedUrl) => {
-  if (parsedUrl.pathname === '/styles/styles.css') {
-    htmlHandler.getCSS(request, response);
-  } else {
-    htmlHandler.getIndex(request, response);
-  }
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getPatterns': jsonHandler.getPattern,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getPatterns': jsonHandler.getPatternMeta,
+    notFound: jsonHandler.notFoundMeta,
+  },
 };
+
+// const handleGet = (request, response, parsedUrl) => {
+//   if (parsedUrl.pathname === '/styles/styles.css') {
+//     htmlHandler.getCSS(request, response);
+//   } else {
+//     htmlHandler.getIndex(request, response);
+//   }
+// };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-  handleGet(request, response, parsedUrl);
+  // handleGet(request, response, parsedUrl);
+
+  if (!urlStruct[request.method]) {
+    return urlStruct['HEAD'].notFound(request, response);
+  }
+
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    return urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    return urlStruct[request.method].notFound(request, response);
+  }
+
 };
 
 http.createServer(onRequest).listen(port, () => {
